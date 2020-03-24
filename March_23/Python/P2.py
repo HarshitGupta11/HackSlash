9f2df285074a2cf946361f649cf75dcf2f146f12
tusharpandey13 <tusharpandey13@gmail.com>
2020-03-24 07:48:10 +0530

    p2 soln + comments

--- March_23/Python/P2.py
+++ March_23/Python/P2.py
@@ -3,8 +3,8 @@
 n_iter = int(input())
 for i in range(n_iter):     # iterate over test cases
     N = int(input())        # take input for N
-	                        # take input of array and save in arr
-	arr = [int(x) for x in input().split()]
+                            # take input of array and save in arr
+    arr = [int(x) for x in input().split()]
     count = 0               # counter var
     for i, e in enumerate(arr): # iterate over arr, with index and val
         for e1 in arr[i:]:  # iterate over elements ahead from current elemnt
