2991e250a7b59a29dc2593db04bc87f247b27db6
tusharpandey13 <tusharpandey13@gmail.com>
2020-03-24 07:42:07 +0530

    p2 soln + comments

--- March_23/Python/P2.py
+++ March_23/Python/P2.py
@@ -1,12 +1,13 @@
 #Finding Pairs - tushar pandey
 
 n_iter = int(input())
-for i in range(n_iter):
-    N = int(input())
-    arr = [int(x) for x in input().split()]
-    count = 0
-    for i, e in enumerate(arr):
-        for e1 in arr[i:]:
-            if e1 == e:
-                count += 1
-    print(count)
+for i in range(n_iter):	# iterate over test cases
+    N = int(input())	# take input for N
+	# take input of array and save in arr
+	arr = [int(x) for x in input().split()]
+    count = 0	# counter var
+    for i, e in enumerate(arr):	# iterate over arr, with index and val
+        for e1 in arr[i:]:	# iterate over elements ahead from current elemnt
+            if e1 == e:		# base condition
+                count += 1	# condition true, increase counter
+    print(count)			# print answer
